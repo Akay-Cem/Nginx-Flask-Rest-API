a336705d03e3e213878041844b0b5195cd19beac
Akay-Cem <cem_akay@icloud.com>
2024-11-02 07:39:13 +0100

    Adding all files

---
+++ app/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, request
+import os
+import mysql.connector
+
+app = Flask(__name__)
+
+def get_db_connection():
+    return mysql.connector.connect(
+            host=os.environ.get('MYSQL_HOST', 'mysql'),
+            user=os.environ.get('MYSQL_USER', 'root'),
+            password=os.environ.get('MYSQL_PASSWORD', 'root'),
+            database=os.environ.get('MYSQL_DATABASE', 'test_db')
+        )
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM data')
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(rows)
+
+@app.route('/api/data', methods=['POST'])
+def add_data():
+    new_data = request.json.get('data')
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO data (data) VALUES (%s)', (new_data,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return jsonify({'message': 'Data added!'}), 201
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
